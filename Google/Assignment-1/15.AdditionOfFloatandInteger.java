1b7265cf3a33e36621c356a552312af5515178cb
charan sri <107253538+CharanSriKrishna@users.noreply.github.com>
2022-09-18 21:40:07 +0530

    Create 15.AdditionOfFloatandInteger.java

---
+++ Google/Assignment-1/15.AdditionOfFloatandInteger.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main
+{  
+  public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        float s = sc.nextFloat();
+        System.out.println((float)(n+s));
+	}
+}
