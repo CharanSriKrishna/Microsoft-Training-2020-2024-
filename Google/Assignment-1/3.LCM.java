b99dd499953f5bcf4d7cf546c7120704ffdae557
charan sri <107253538+CharanSriKrishna@users.noreply.github.com>
2022-09-18 21:04:05 +0530

    Create 3.LCM.java

---
+++ Google/Assignment-1/3.LCM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Main
+{   
+    public static int GCD(int a,int b){
+        int gcd=1;
+        for(int i = 1; i <= a && i <= b; i++)  
+	      {  
+		      if(a%i==0 && b%i==0)  
+		      gcd = i;  
+	      }  
+	      return gcd;
+    }
+    public static int LCM(int a,int b){
+        return (a/GCD(a,b))*b;
+    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(LCM(a,b));
+	}
+}
