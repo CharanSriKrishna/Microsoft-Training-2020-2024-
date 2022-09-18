67f58a348813a55f44d26a9a0e5c9d990ca3ffc3
charan sri <107253538+CharanSriKrishna@users.noreply.github.com>
2022-09-18 21:06:34 +0530

    Update 4.AreaOfTheTriangle.java

--- Google/Assignment-1/4.AreaOfTheTriangle.java
+++ Google/Assignment-1/4.AreaOfTheTriangle.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 public class Main
 {   
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int x1 = scanner.nextInt();
-		int y1 = scanner.nextInt();
-		int x2 = scanner.nextInt();
-		int y2 = scanner.nextInt();
-		int x3 = scanner.nextInt();
-		int y3 = scanner.nextInt();
+		Scanner sc = new Scanner(System.in);
+		int x1 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y2 = sc.nextInt();
+		int x3 = sc.nextInt();
+		int y3 = sc.nextInt();
 		double area1 = Math.abs(x1*(y2-y3)+x2*(y1-y3)+x3*(y1-y2));
 		System.out.println(area1/2);
 		double area2 = Math.abs(x1*(y1-y2)+x2*(y1-y3)+x3*(y1-y2));
