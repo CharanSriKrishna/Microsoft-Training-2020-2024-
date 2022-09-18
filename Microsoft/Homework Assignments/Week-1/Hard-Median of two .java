ebc55ecb2f5f8fd9e156d4b107db73107abeed09
charan sri <107253538+CharanSriKrishna@users.noreply.github.com>
2022-09-18 20:30:20 +0530

    Create Hard-Median of two .java

---
+++ Microsoft/Homework Assignments/Week-1/Hard-Median of two .java
@@ -0,0 +1,33 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int k=nums1.length+nums2.length;
+        int [] nums3=new int[k];
+        int j=0,m=0;
+        for(int i=0;i<k;i++){
+            if(m==nums1.length){
+                while(i<k){
+                    nums3[i]=nums2[j];
+                    j++;
+                    i++;
+                }
+            }
+            else if(j==nums2.length){
+                while(i<k){
+                    nums3[i]=nums1[m];
+                    m++;
+                    i++;
+                }
+            }
+            else if(nums1[m]<nums2[j]){
+                nums3[i]=nums1[m];
+                m++;
+            }
+            else{
+                nums3[i]=nums2[j];
+                j++;
+            }
+        }
+        if(k%2==1) return nums3[k/2];
+        else return (nums3[k/2]+nums3[(k/2)-1])/2.0;
+    }
+}
