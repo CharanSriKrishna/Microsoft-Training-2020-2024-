1752e90f32fbd6f4103f0fd37861b83dc853eac5
charan sri <107253538+CharanSriKrishna@users.noreply.github.com>
2022-09-18 21:02:11 +0530

    Update 2.GCD.java

--- Google/Assignment-1/2.GCD.java
+++ Google/Assignment-1/2.GCD.java
@@ -2,10 +2,13 @@ import java.util.Scanner;
 public class Main
 {   
     public static int GCD(int a,int b){
-        if(a==0){
-            return b;
-        }
-        return GCD(b%a,a);
+	    int gcd=1;
+        for(int i = 1; i <= a && i <= b; i++)  
+	{  
+		if(a%i==0 && b%i==0)  
+		gcd = i;  
+	}  
+	return gcd;
     }
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
